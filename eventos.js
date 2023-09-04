abbc79946a0e47f276f96be48cbd98d6d7cc6810
taliasouza <talysouza10@outlook.com>
2023-09-03 22:46:34 -0300

    Sitio web creado

---
+++ eventos.js
@@ -0,0 +1,11 @@
+
+var miBoton = document.getElementById('miBoton');
+var miDiv = document.getElementById('miDiv');
+
+miBoton.addEventListener('click', function () {
+    alert('Hola!');
+});
+
+miDiv.addEventListener('click', function () {
+    alert('Hola! Soy el div');
+});
